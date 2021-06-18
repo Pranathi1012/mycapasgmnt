3a274bf9e59889feb9f20af0020316c0ac292640
Pranathi1012 <85788425+Pranathi1012@users.noreply.github.com>
2021-06-18 11:13:51 +0530

    Add files via upload

---
+++ mycapasgnmt1.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+print("list1",list1)
+for num in list1:
+    if num >= 0:
+        print("positive numbers in list1:",num)
+list2=[12,14,-95,3]
+print("list2",list2)
+for num in list2:
+    if num >= 0:
+        print("positive numbers in list2:",num)
